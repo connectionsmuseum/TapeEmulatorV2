91b60b781022bd424660933d4469f1410ec4866c
Colin Slater <ctslater@uw.edu>
2020-06-28 14:58:21 -0700

    Remove USB composite, just USB CDC ACM.

--- usb_start.c
+++ usb_start.c
@@ -8,89 +8,114 @@
 #include "atmel_start.h"
 #include "usb_start.h"
 
-/* Max LUN number */
-#define CONF_USB_MSC_MAX_LUN 0
-
 #if CONF_USBD_HS_SP
-static uint8_t multi_desc_bytes[] = {
+static uint8_t single_desc_bytes[] = {
     /* Device descriptors and Configuration descriptors list. */
-    COMPOSITE_HS_DESCES_LS_FS};
-static uint8_t multi_desc_bytes_hs[] = {
+    CDCD_ACM_HS_DESCES_LS_FS};
+static uint8_t single_desc_bytes_hs[] = {
     /* Device descriptors and Configuration descriptors list. */
-    COMPOSITE_HS_DESCES_HS};
+    CDCD_ACM_HS_DESCES_HS};
+#define CDCD_ECHO_BUF_SIZ CONF_USB_CDCD_ACM_DATA_BULKIN_MAXPKSZ_HS
 #else
-static uint8_t multi_desc_bytes[] = {
+static uint8_t single_desc_bytes[] = {
     /* Device descriptors and Configuration descriptors list. */
-    COMPOSITE_DESCES_LS_FS};
+    CDCD_ACM_DESCES_LS_FS};
+#define CDCD_ECHO_BUF_SIZ CONF_USB_CDCD_ACM_DATA_BULKIN_MAXPKSZ
 #endif
 
-static struct usbd_descriptors multi_desc[] = {{multi_desc_bytes, multi_desc_bytes + sizeof(multi_desc_bytes)}
+static struct usbd_descriptors single_desc[]
+    = {{single_desc_bytes, single_desc_bytes + sizeof(single_desc_bytes)}
 #if CONF_USBD_HS_SP
-                                               ,
-                                               {multi_desc_bytes_hs, multi_desc_bytes_hs + sizeof(multi_desc_bytes_hs)}
+       ,
+       {single_desc_bytes_hs, single_desc_bytes_hs + sizeof(single_desc_bytes_hs)}
 #endif
 };
+
+/** Buffers to receive and echo the communication bytes. */
+static uint32_t usbd_cdc_buffer[CDCD_ECHO_BUF_SIZ / 4];
 
 /** Ctrl endpoint buffer */
 static uint8_t ctrl_buffer[64];
 
-void composite_device_init(void)
+/**
+ * \brief Callback invoked when bulk OUT data received
+ */
+static bool usb_device_cb_bulk_out(const uint8_t ep, const enum usb_xfer_code rc, const uint32_t count)
+{
+	cdcdf_acm_write((uint8_t *)usbd_cdc_buffer, count);
+
+	/* No error. */
+	return false;
+}
+
+/**
+ * \brief Callback invoked when bulk IN data received
+ */
+static bool usb_device_cb_bulk_in(const uint8_t ep, const enum usb_xfer_code rc, const uint32_t count)
+{
+	/* Echo data. */
+	cdcdf_acm_read((uint8_t *)usbd_cdc_buffer, sizeof(usbd_cdc_buffer));
+
+	/* No error. */
+	return false;
+}
+
+/**
+ * \brief Callback invoked when Line State Change
+ */
+static bool usb_device_cb_state_c(usb_cdc_control_signal_t state)
+{
+	if (state.rs232.DTR) {
+		/* Callbacks must be registered after endpoint allocation */
+		cdcdf_acm_register_callback(CDCDF_ACM_CB_READ, (FUNC_PTR)usb_device_cb_bulk_out);
+		cdcdf_acm_register_callback(CDCDF_ACM_CB_WRITE, (FUNC_PTR)usb_device_cb_bulk_in);
+		/* Start Rx */
+		cdcdf_acm_read((uint8_t *)usbd_cdc_buffer, sizeof(usbd_cdc_buffer));
+	}
+
+	/* No error. */
+	return false;
+}
+
+/**
+ * \brief CDC ACM Init
+ */
+void cdc_device_acm_init(void)
 {
 	/* usb stack init */
 	usbdc_init(ctrl_buffer);
 
 	/* usbdc_register_funcion inside */
-#if CONF_USB_COMPOSITE_CDC_ACM_EN
 	cdcdf_acm_init();
-#endif
-#if CONF_USB_COMPOSITE_HID_MOUSE_EN
-	hiddf_mouse_init();
-#endif
-#if CONF_USB_COMPOSITE_HID_KEYBOARD_EN
-	hiddf_keyboard_init();
-#endif
-#if CONF_USB_COMPOSITE_MSC_EN
-	mscdf_init(CONF_USB_MSC_MAX_LUN);
-#endif
-}
 
-void composite_device_start(void)
-{
-	usbdc_start(multi_desc);
+	usbdc_start(single_desc);
 	usbdc_attach();
 }
 
-void composite_device_example(void)
+/**
+ * Example of using CDC ACM Function.
+ * \note
+ * In this example, we will use a PC as a USB host:
+ * - Connect the DEBUG USB on XPLAINED board to PC for program download.
+ * - Connect the TARGET USB on XPLAINED board to PC for running program.
+ * The application will behave as a virtual COM.
+ * - Open a HyperTerminal or other COM tools in PC side.
+ * - Send out a character or string and it will echo the content received.
+ */
+void cdcd_acm_example(void)
 {
+	while (!cdcdf_acm_is_enabled()) {
+		// wait cdc acm to be installed
+	};
 
-	/* Initialize */
-	/* It's done with system init ... */
+	cdcdf_acm_register_callback(CDCDF_ACM_CB_STATE_C, (FUNC_PTR)usb_device_cb_state_c);
 
-	/* Before start do function related initializations */
-	/* Add your code here ... */
-
-	/* Start device */
-	composite_device_start();
-
-	/* Main loop */
 	while (1) {
-		if (cdcdf_acm_is_enabled()) {
-			/* CDC ACM process*/
-		}
-		if (hiddf_mouse_is_enabled()) {
-			/* HID Mouse process */
-		}
-		if (hiddf_keyboard_is_enabled()) {
-			/* HID Keyboard process */
-		};
-		if (mscdf_is_enabled()) {
-			/* MSC process */
-		}
 	}
 }
 
 void usb_init(void)
 {
 
-	composite_device_init();
+	cdc_device_acm_init();
 }
